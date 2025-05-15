b6f643b6f6ec36640e380a0b868eb1d0741ad99c
tony13382 <ttsmcpe@gmail.com>
2025-05-15 20:22:01 +0800

    新增 formattor 和 retriever 模組，實作數據處理功能

---
+++ formattor.py
@@ -0,0 +1,93 @@
+def press_title(data):
+    if not data:
+        raise ValueError("Data must be provided")
+    if data.get("type") != "title":
+        raise ValueError("Data type must be 'title'")
+    titles = data.get("title")
+    title_str = ""
+    index = 0
+    for title in titles:
+        if title.get("type") == "text":
+            text = title.get("text")
+            if text:
+                content = text.get("content")
+                if content:
+                    if index == 0:
+                        title_str += f"{content}"
+                    else:
+                        title_str += f"\n{content}"
+                    index += 1
+    return title_str
+
+
+def press_text(data):
+    if not data:
+        raise ValueError("Data must be provided")
+    if data.get("type") != "rich_text":
+        raise ValueError("Data type must be 'rich_text'")
+    texts = data.get("rich_text")
+    str = ""
+    index = 0
+    for text in texts:
+        text = text.get("text")
+        if text:
+            content = text.get("content")
+            if content:
+                if index == 0:
+                    str += f"{content}"
+                else:
+                    str += f"\n{content}"
+                index += 1
+    return str
+
+
+def press_number(data):
+    if not data:
+        raise ValueError("Data must be provided")
+    if data.get("type") != "number":
+        raise ValueError("Data type must be 'number'")
+    number = data.get("number")
+    return number
+
+
+def press_url(data):
+    if not data:
+        raise ValueError("Data must be provided")
+    if data.get("type") != "url":
+        raise ValueError("Data type must be 'url'")
+    url = data.get("url")
+    return url
+
+
+def press_files(data):
+    if not data:
+        raise ValueError("Data must be provided")
+    if data.get("type") != "files":
+        raise ValueError("Data type must be 'files'")
+    files = data.get("files")
+    paths = []
+    for file in files:
+        if file.get("type") == "external":
+            path = file.get("external", {}).get("url")
+        elif file.get("type") == "file":
+            path = file.get("file", {}).get("url")
+
+        if path:
+            paths.append(path)
+
+    return paths
+
+
+def press_file(data):
+    if not data:
+        raise ValueError("Data must be provided")
+    if data.get("type") != "file":
+        raise ValueError("Data type must be 'files'")
+    path = None
+    if data.get("type") == "external":
+        path = data.get("external", {}).get("url")
+    elif data.get("type") == "file":
+        path = data.get("file", {}).get("url")
+    else:
+        raise ValueError(f"File type must be 'external' or 'file'\n {data}")
+    return path
---
+++ retriever.py
@@ -0,0 +1,63 @@
+import requests
+
+
+def notion_query(database_id: str, token: str, only_first_page: bool = True):
+    if not database_id or not token:
+        raise ValueError("Database ID and token must be provided")
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+    baer_token = f"Bearer {token}"
+    headers = {
+        "Notion-Version": "2021-05-13",
+        "Content-Type": "application/json",
+    }
+    response = requests.post(
+        url,
+        headers={
+            "Authorization": baer_token,
+            **headers,
+        },
+    )
+    if response.status_code != 200:
+        raise Exception(f"Error: {response.status_code} - {response.text}")
+    data = response.json()
+    if only_first_page:
+        return data.get("results", [])
+    else:
+        all_results = data.get("results", [])
+        while data.get("has_more"):
+            next_cursor = data.get("next_cursor")
+            response = requests.post(
+                url,
+                headers={
+                    "Authorization": baer_token,
+                    **headers,
+                },
+                json={"start_cursor": next_cursor},
+            )
+            if response.status_code != 200:
+                raise Exception(f"Error: {response.status_code} - {response.text}")
+            data = response.json()
+            all_results.extend(data.get("results", []))
+        return all_results
+
+
+def notion_page(page_id: str, token: str):
+    if not page_id or not token:
+        raise ValueError("Page ID and token must be provided")
+    url = f"https://api.notion.com/v1/blocks/{page_id}/children"
+    baer_token = f"Bearer {token}"
+    headers = {
+        "Notion-Version": "2021-05-13",
+        "Content-Type": "application/json",
+    }
+    response = requests.get(
+        url,
+        headers={
+            "Authorization": baer_token,
+            **headers,
+        },
+    )
+    if response.status_code != 200:
+        raise Exception(f"Error: {response.status_code} - {response.text}")
+    data = response.json()
+    return data.get("results", [])
